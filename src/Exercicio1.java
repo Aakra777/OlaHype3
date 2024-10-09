7f05538ad93c72f198ccd2e024e66b8f7912c304
Aakra777 <b.carsil.07@gmail.com>
2024-10-09 12:25:10 -0300

    https://github.com/Aakra777/OlaHype3.git

---
+++ src/Exercicio1.java
@@ -0,0 +1,9 @@
+public class Exercicio1 {
+    public static void main (String[] args) {
+        String brendon = "Brendon";
+        String endereco = "Xoxotopolis" ;
+        double salario = 3500.55;
+        String dataSalario = "05/07/2025" ;
+        System.out.println("Eu "+brendon+", morando no endereço de "+ endereco +", confirmo que rececibi o salário de "+salario+", na data "+dataSalario);
+    }
+}
--- src/OlaHype3.java
+++ src/OlaHype3.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
 
 public class OlaHype3 {
+    public static void main(String[] args){
+        System.out.println("Olá Hype3") ;
+    }
 }
